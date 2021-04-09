e2cd6f06cb47fc1c252fbc55383794804b86ab3f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-04-08 19:10:24 -0700

    Replace CPP png with SVG

--- info.js
+++ info.js
@@ -1,14 +1,14 @@
-import path from 'path'
+import path from "path";
 
 export const name = "Scoder12";
 export const siteTitle = "Scoder12's Blog";
-export const postsDirectory = path.join(process.cwd(), 'posts');
-export const profileImage = '/imgs/profile.png';
+export const postsDirectory = path.join(process.cwd(), "posts");
+export const profileImage = "/imgs/profile.png";
 export const tech = {
-	"Python": "python.png",
-	"NodeJS": "nodejs.png",
-	"Web": "web.png",
-	"React": "react.png",
-	"Next.js": "nextjs.png",
-	"C / C++": "cpp.png"
+  Python: "python.png",
+  NodeJS: "nodejs.png",
+  Web: "web.png",
+  React: "react.png",
+  "Next.js": "nextjs.png",
+  "C / C++": "cpp.svg",
 };
